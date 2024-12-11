c9d6a670a6c2b0c696af27e92ab44670104e8fd1
Leandro ferreria machado <adswebteste@gmail.com>
2024-12-10 22:19:45 -0300

    Começando no COBOL

---
+++ PROGRAMA1.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA1.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+      *> cobol-lint CL002 main-procedure
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM PROGRAMA1.
